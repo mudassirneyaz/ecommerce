9d9ad4bb30ccf8b6ff45833412bc9ef1631fb0b7
mudassirneyaz <mudassirneyaz000@gmail.com>
2021-02-13 09:56:32 +0000

    Add Name

--- home.php
+++ home.php
@@ -13,7 +13,7 @@
               <div class="item active">
                 <div class="col-sm-6">
                   <h1><span>E</span>-SHOPPER</h1>
-                  <h2>Free E-Commerce Template</h2>
+                  <h2>E-Commerce Template From Mudassir</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                  
                 </div>
